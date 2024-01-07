491dd02f4665373e38f071cebf111a0d7e036252
Yarmolka <apy242@gmail.com>
2024-01-07 21:30:56 +0200

    Lesson 10: Aura Player Controller

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -6,12 +6,21 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
-/**
- * 
- */
+class UInputMappingContext;
+
 UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	AAuraPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
 	
 };
