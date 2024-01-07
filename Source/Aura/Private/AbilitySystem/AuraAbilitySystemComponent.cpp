871bef3e2834eb24250e7682b785bd06c8982f0f
Yarmolka <apy242@gmail.com>
2024-01-08 01:01:35 +0200

    Lesson 19: Ability System Component and Attribute Set

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -0,0 +1,5 @@
+// Copyright Aura
+
+
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+
---
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -0,0 +1,17 @@
+// Copyright Aura
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AuraAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
