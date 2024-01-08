34dbbb077a338014603c9589f94e016a9e1628fb
Yarmolka <apy242@gmail.com>
2024-01-08 19:47:49 +0200

    Lesson 29: Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright Aura
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
---
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -0,0 +1,28 @@
+// Copyright Aura
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraWidgetController.generated.h"
+
+class UAbilitySystemComponent;
+class UAttributeSet;
+
+UCLASS()
+class AURA_API UAuraWidgetController : public UObject
+{
+	GENERATED_BODY()
+protected:
+
+	UPROPERTY(BlueprintReadOnly, Category="WidgetController")
+	TObjectPtr<APlayerController> PlayerController;
+
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
+	TObjectPtr<APlayerState> PlayerState;
+
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
+	TObjectPtr<UAttributeSet> AttributeSet;
+};
