4ffa0e60f6de354221dd1a63f847daf6c747d4d6
Yarmolka <apy242@gmail.com>
2024-01-14 18:52:31 +0200

    Lesson 118: Receiving Target Data

--- Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
+++ Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
@@ -24,4 +24,6 @@ public:
 private:
 	virtual void Activate() override;
 	void SendMouseCursorData();
+
+	void OnTargetDataReplicatedCallback(const FGameplayAbilityTargetDataHandle& DataHandle, FGameplayTag ActivationTag);
 };
