8c25d96ea54095b112cf4cc7ca5ae2faa2c51bbf
Yarmolka <apy242@gmail.com>
2024-01-16 14:31:29 +0200

    Lesson 140: Floating Text Widget

---
+++ Source/Aura/Private/UI/Widget/DamageTextComponent.cpp
@@ -0,0 +1,5 @@
+// Copyright Aura
+
+
+#include "UI/Widget/DamageTextComponent.h"
+
---
+++ Source/Aura/Public/UI/Widget/DamageTextComponent.h
@@ -0,0 +1,22 @@
+// Copyright Aura
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "DamageTextComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UDamageTextComponent : public UWidgetComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void SetDamageText(float Damage);
+	
+};
