c321843046f7940abd4a5fce28e0062f00d9e777
Yarmolka <apy242@gmail.com>
2024-01-18 22:45:18 +0200

    Lesson 166: Blackboard Keys

--- Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
+++ Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
@@ -2,13 +2,17 @@
 
 
 #include "AI/BTService_FindNearestPlayer.h"
-
 #include "AIController.h"
+#include "Kismet/GameplayStatics.h"
 
 void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
 
-	GEngine->AddOnScreenDebugMessage(1, 3.f, FColor::Red, *AIOwner->GetName());
-	GEngine->AddOnScreenDebugMessage(2, 3.f, FColor::Green, *ActorOwner->GetName());
+	APawn* OwningPawn = AIOwner->GetPawn();
+
+	const FName TargetTag = OwningPawn->ActorHasTag(FName("Player")) ? FName("Enemy") : FName("Player");
+
+	TArray<AActor*> ActorsWithTag;
+	UGameplayStatics::GetAllActorsWithTag(OwningPawn, TargetTag, ActorsWithTag);
 }
