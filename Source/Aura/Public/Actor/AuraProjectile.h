fa99c4b098aa698d0cbd9cf5d692a76ba534c1a6
Yarmolka <apy242@gmail.com>
2024-01-22 23:56:08 +0200

    Lesson 225: Elementalist Behavior Tree

--- Source/Aura/Public/Actor/AuraProjectile.h
+++ Source/Aura/Public/Actor/AuraProjectile.h
@@ -39,7 +39,7 @@ private:
 
 	bool bHit = false;
 
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<USphereComponent> Sphere;
 
 	UPROPERTY(EditAnywhere)
