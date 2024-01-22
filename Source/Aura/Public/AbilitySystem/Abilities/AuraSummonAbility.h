362eb2e56a8d909a2e4bab7b949c80e942362d23
Yarmolka <apy242@gmail.com>
2024-01-22 19:39:40 +0200

    Lesson 219: Shaman Summon Locations

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraSummonAbility.h
@@ -0,0 +1,36 @@
+// Copyright Aura
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraGameplayAbilityBase.h"
+#include "AuraSummonAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraSummonAbility : public UAuraGameplayAbilityBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	TArray<FVector> GetSpawnLocations();
+
+	UPROPERTY(EditDefaultsOnly, Category="Summoning")
+	int32 NumMinions = 5;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	TArray<TSubclassOf<APawn>> MinionClasses;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	float MinSpawnDistance = 50.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	float MaxSpawnDistance = 250.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	float SpawnSpread = 90.f;
+};
